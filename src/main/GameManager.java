616be9efc45b2f4472c18b3b695e105744bc7dcf
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-26 21:46:38 +0700

    add the frame delay to the game loop and fix some parts of the file

--- src/main/GameManager.java
+++ src/main/GameManager.java
@@ -4,70 +4,82 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class GameManager extends JPanel implements Runnable
-{
+public class GameManager extends JPanel implements Runnable {
     private static volatile GameManager gameManager;
     private Thread gameThread;
     private final int width = 500;
     private final int height = 500;
     private final Color backgroundColor = Color.white;
+    private final int FPS = 60;
+    private boolean running;
 
-    public static GameManager getInstance()
-    {
-        if(gameManager == null)
-        {
-            synchronized(GameManager.class)
-            {
-                if(gameManager == null)
+    // the minimum nanosecond at each frame
+    private final double frameTime = 1_000_000_000.0 / FPS;
+
+    public static GameManager getInstance() {
+        if (gameManager == null) {
+            synchronized (GameManager.class) {
+                if (gameManager == null)
                     gameManager = new GameManager();
             }
         }
         return gameManager;
     }
 
-    private GameManager()
-    {
+    private GameManager() {
         this.setPreferredSize(new Dimension(width, height));
         this.setBackground(backgroundColor);
         this.setDoubleBuffered(true);
         this.setFocusable(true);
     }
 
-    public void startGame()
-    {
-        gameThread = new Thread(this);
-        gameThread.start();
+    public void startGame() {
+        if (gameThread == null || !running) {
+            running = true;
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+    }
+
+    public void stopGame() {
+        running = false;
     }
 
     @Override
-    public void run()
-    {
-        while(gameThread != null)
-        {
+    public void run() {
+
+        while (running) {
+
+            long startFrame = System.nanoTime();
+
             updateGame();
             repaint();
+
+            long endFrame = System.nanoTime();
+            long elapsed = endFrame - startFrame;
+            long sleepTime = (long) (frameTime - elapsed);
+
+            if (sleepTime > 0) {
+                try {
+                    // convert sleepTime from millisecond to nanosecond
+                    Thread.sleep(sleepTime / 1_000_000, (int) (sleepTime % 1_000_000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    public void updateGame()
-    {
-
-    }
-
-    public void renderGame(Graphics graphic2D)
-    {
-
-    }
-
-    public void paintComponent(Graphics graphic)
-    {
+    public void paintComponent(Graphics graphic) {
         super.paintComponent(graphic);
-        Graphics2D graphic2D = (Graphics2D) graphic.create();
+        renderGame((Graphics2D) graphic);
+    }
 
-        try {
-            renderGame(graphic2D);
-        } finally {
-            graphic2D.dispose();
-        }
+    public void updateGame() {
+
+    }
+
+    public void renderGame(Graphics graphic2D) {
+
     }
 }

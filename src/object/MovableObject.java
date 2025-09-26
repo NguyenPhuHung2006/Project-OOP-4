3501657034e67931f2fa9842777de659b105cc7d
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-27 00:17:57 +0700

    initialize the GameObject and MovableObject

---
+++ src/object/MovableObject.java
@@ -0,0 +1,26 @@
+package object;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MovableObject extends GameObject {
+
+    private int dx;
+    private int dy;
+    private int speed;
+
+    MovableObject(int x, int y, int width, int height, BufferedImage texture, int speed) {
+        super(x, y, width, height, texture);
+        this.speed = speed;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+}

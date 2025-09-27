6b2ff950701b428d5c1b751584d8b0499ec93d29
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-27 23:40:31 +0700

    make the MovableObject class to be an abstract class

--- src/object/MovableObject.java
+++ src/object/MovableObject.java
@@ -3,24 +3,18 @@ package object;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class MovableObject extends GameObject {
+public abstract class MovableObject extends GameObject {
 
     private int dx;
     private int dy;
     private int speed;
 
-    MovableObject(int x, int y, int width, int height, BufferedImage texture, int speed) {
+    MovableObject(int x, int y, int width, int height, int speed, BufferedImage texture) {
         super(x, y, width, height, texture);
         this.speed = speed;
+        dx = 0;
+        dy = 0;
     }
 
-    @Override
-    public void update() {
-
-    }
-
-    @Override
-    public void render(Graphics2D graphics) {
-
-    }
+    public abstract void move();
 }

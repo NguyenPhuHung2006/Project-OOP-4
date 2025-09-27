e7993f9823a9990790fa67bb6a7ab8805fba174f
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-27 16:48:09 +0700

    add methods that create texture and scale texture in the GameObject class

--- src/object/GameObject.java
+++ src/object/GameObject.java
@@ -1,5 +1,6 @@
 package object;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public abstract class GameObject {
@@ -10,15 +11,39 @@ public abstract class GameObject {
     protected int height;
     protected BufferedImage texture;
 
-    GameObject(int x, int y, int width, int height, BufferedImage texture) {
+    private static BufferedImage scaleTexture(BufferedImage originalTexture, int width, int height) {
+        if (originalTexture == null) {
+            return null;
+        }
+
+        BufferedImage scaledTexture = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics2D = scaledTexture.createGraphics();
+
+        graphics2D.drawImage(originalTexture.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
+        graphics2D.dispose();
+
+        return scaledTexture;
+    }
+
+    public GameObject(int x, int y, int width, int height, BufferedImage texture) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.texture = texture;
+        this.texture = scaleTexture(texture, width, height);
+    }
+
+    public void render(Graphics2D graphics2D) {
+        if (texture != null && graphics2D != null) {
+            graphics2D.drawImage(texture, x, y, null);
+        }
+    }
+
+    public void render(Graphics2D graphics2D, int scaledWidth, int scaledHeight) {
+        if (texture != null && graphics2D != null) {
+            graphics2D.drawImage(texture, x, y, scaledWidth, scaledHeight, null);
+        }
     }
 
     public abstract void update();
-
-    public abstract void render(java.awt.Graphics2D graphics);
 }

b04156659b44f99ebdd8712d9db6b9459105a462
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-27 14:55:40 +0700

    Implement basic audio support (Sound, SoundManager, SoundEffect)

---
+++ src/main/SoundEffect.java
@@ -0,0 +1,19 @@
+package main;
+
+public enum SoundEffect {
+
+    // these are only used to test by Hung, we can change this once we get further
+    BOUNCE("/assets/bounce.wav"),
+    BRICK("/assets/brick.wav"),
+    MUSIC("/asset/chest_open.wav");
+
+    private final String path;
+
+    SoundEffect(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}

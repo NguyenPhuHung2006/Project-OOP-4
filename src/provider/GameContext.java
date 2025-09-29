0a0bd87e4165941227bdf7f3b6492ce4e5cab3a7
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-29 11:17:58 +0700

    add Paddle to the GameContext

--- src/provider/GameContext.java
+++ src/provider/GameContext.java
@@ -1,12 +1,16 @@
 package provider;
 
+import object.Paddle;
+
 public class GameContext {
     private final InputProvider input;
     private final BoundProvider bound;
+    private final Paddle paddle;
 
-    public GameContext(InputProvider input, BoundProvider bound) {
+    public GameContext(InputProvider input, BoundProvider bound, Paddle paddle) {
         this.input = input;
         this.bound = bound;
+        this.paddle = paddle;
     }
 
     public InputProvider getInput() {
@@ -16,4 +20,6 @@ public class GameContext {
     public BoundProvider getBounds() {
         return bound;
     }
+
+    public Paddle getPaddle() { return paddle; }
 }

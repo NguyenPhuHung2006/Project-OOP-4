9e30e1124a10bd26ea7bba6265e93b16e1c48f95
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-09-30 20:26:15 +0700

    add Intersect object detector

--- src/utils/IntersectUtils.java
+++ src/utils/IntersectUtils.java
@@ -4,9 +4,9 @@ import object.GameObject;
 
 public class IntersectUtils {
     public static boolean intersect(GameObject a, GameObject b) {
-        return  a.getX() < b.getX() + b.getWidth() &&   // A.left < B.right
-                a.getX() + a.getWidth() > b.getX() &&  // A.right > B.left
-                a.getY() < b.getY() + b.getHeight() && // A.top < B.bottom
-                a.getY() + a.getHeight() > b.getY();   // A.bottom > B.top
+        return  a.getX() <= b.getX() + b.getWidth() &&   // A.left < B.right
+                a.getX() + a.getWidth() >= b.getX() &&  // A.right > B.left
+                a.getY() <= b.getY() + b.getHeight() && // A.top < B.bottom
+                a.getY() + a.getHeight() >= b.getY();   // A.bottom > B.top
     }
 }

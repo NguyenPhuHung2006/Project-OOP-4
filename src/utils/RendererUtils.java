111514ddda397dc07205995f216eaa389aa76453
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-10-02 00:56:30 +0700

    initialize the RendererUtils class that helps render objects of the game

---
+++ src/utils/RendererUtils.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class RendererUtils {
+
+    public static void render(BufferedImage texture, int x, int y, Graphics2D graphics2D) {
+        if (texture != null && graphics2D != null) {
+            graphics2D.drawImage(texture, x, y, null);
+        }
+    }
+
+    public static void render(BufferedImage texture, int x, int y, int scaledWidth, int scaledHeight, Graphics2D graphics2D) {
+        if (texture != null && graphics2D != null) {
+            graphics2D.drawImage(texture, x, y, scaledWidth, scaledHeight, null);
+        }
+    }
+
+}

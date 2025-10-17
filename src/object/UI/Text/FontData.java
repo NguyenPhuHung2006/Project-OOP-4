17ce17fb5199ee75a2446b9ef14942974626edc7
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2025-10-17 20:57:03 +0700

    Apply new font to the game

--- src/object/UI/Text/FontData.java
+++ src/object/UI/Text/FontData.java
@@ -1,9 +1,13 @@
 package object.UI.Text;
 
+import exception.ExceptionHandler;
+
 import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class FontData {
-    public String name;
+    public String fontPath;
     public String style;
     public int size;
 
@@ -11,8 +15,18 @@ public class FontData {
         int fontStyle = switch (style.toUpperCase()) {
             case "BOLD" -> Font.BOLD;
             case "ITALIC" -> Font.ITALIC;
+            case "BOLD_ITALIC" -> Font.BOLD | Font.ITALIC;
             default -> Font.PLAIN;
         };
-        return new Font(name, fontStyle, size);
+
+        try {
+            Font baseFont = Font.createFont(Font.TRUETYPE_FONT, Objects.requireNonNull(getClass().getResourceAsStream(fontPath)));
+
+            return baseFont.deriveFont(fontStyle, (float) size);
+
+        } catch (IOException | FontFormatException e) {
+            ExceptionHandler.handle(e);
+            return null;
+        }
     }
 }
--- src/object/movable/powerup/SlowBallPowerUp.java
+++ src/object/movable/powerup/SlowBallPowerUp.java
@@ -19,7 +19,6 @@ public class SlowBallPowerUp extends PowerUp {
         Ball ball = gameContext.getBall();
         originalSpeed = ball.getSpeed();
         ball.setSpeed(originalSpeed * slowMultiplier);
-        System.out.println(originalSpeed + " " + ball.getSpeed());
     }
 
     @Override
